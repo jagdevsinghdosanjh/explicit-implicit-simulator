34f0899ea9f6fc4bc7aba9f35f3921933587153b
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-08-17 00:25:18 +0530

    Ok

--- utils/references.py
+++ utils/references.py
@@ -7,7 +7,7 @@ def render_reference_tab():
     references = {
         "Numerical Methods for Partial Differential Equations": "https://ocw.mit.edu/courses/18-336-numerical-methods-for-partial-differential-equations-spring-2009/",
         "Finite Difference Methods for ODEs and PDEs – LeVeque": "https://faculty.washington.edu/rjl/fdmbook/",
-        "Numerical Methods for Engineers – Chapra & Canale": "https://www.academia.edu/42140879/Numerical_Methods_for_Engineers_7th_Edition",
+        "Numerical Methods for Engineers – Chapra & Canale": "https://drive.google.com/file/d/1h0TOgQN1b5EcnHyENeZiWUgfq4cSiOtM/view",
         "Stability Theory – Trefethen Lecture Notes": "https://people.maths.ox.ac.uk/trefethen/lectures.html",
         "Symbolic Computation and Geometry – Springer": "https://link.springer.com/book/10.1007/978-3-030-20206-2",
         "Ancient Geometry and Philosophy – Euclid’s Elements": "https://mathcs.clarku.edu/~djoyce/elements/Euclid.html"

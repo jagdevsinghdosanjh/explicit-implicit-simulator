6966c44073eb2b6ec131be14fe4009b24609fd20
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-08-17 00:12:12 +0530

    Ok

--- utils/references.py
+++ utils/references.py
@@ -8,7 +8,9 @@ def render_reference_tab():
         #"Numerical Analysis by Burden & Faires:"https://mathcs.clarku.edu/~djoyce/numerical-analysis/",
         "Numerical Methods for Partial Differential Equations": "https://ocw.mit.edu/courses/18-336-numerical-methods-for-partial-differential-equations-spring-2009/",
         "Finite Difference Methods for ODEs and PDEs – LeVeque": "https://faculty.washington.edu/rjl/fdmbook/",
-        "Numerical Methods for Engineers – Chapra & Canale": "https://www.academia.edu/42140879/Numerical_Methods_for_Engineers_7th_Edition",
+        #"Numerical Methods for Engineers – Chapra & Canale": "https://www.academia.edu/42140879/Numerical_Methods_for_Engineers_7th_Edition",
+        "Numerical Methods for Engineers – Chapra & Canale": "/assets/books/Numerical_Methods_for_Engineers_7th_Edition_Steven C Chapra_Raymond P Canale.pdf",
+        #C:\Users\jagdevsinghdosanjh.RHINO\explicit-implicit-simulator\assets\books\Numerical_Methods_for_Engineers_7th_Edition_Steven C Chapra_Raymond P Canale.pdf
         "Stability Theory – Trefethen Lecture Notes": "https://people.maths.ox.ac.uk/trefethen/lectures.html",
         "Symbolic Computation and Geometry – Springer": "https://link.springer.com/book/10.1007/978-3-030-20206-2",
         "Ancient Geometry and Philosophy – Euclid’s Elements": "https://mathcs.clarku.edu/~djoyce/elements/Euclid.html"

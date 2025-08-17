5954717d6516c0724cbefd559d231bea56fca88d
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-08-17 13:36:04 +0530

    Ok

--- utils/philosophy.py
+++ utils/philosophy.py
@@ -53,10 +53,4 @@ def render_philosophy_tab():
     - Anyone who wonders how we model change
     """)
 
-    st.markdown("### 🌌 Final Thought")
-    st.write("""
-    This app is a bridge—between intuition and rigor, sunlight and moonlight, breath and computation.  
-    Welcome to a universe where math becomes myth. Myth a traditional story, especially one 
-    concerning the early history of a people or explaining a natural or social phenomenon, 
-    and typically involving supernatural beings or events. Real but Unknown to Visible Exitence.
-    """)
+    
